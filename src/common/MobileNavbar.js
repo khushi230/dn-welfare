0ca96dd9da63147e8329d49871da245502b7fbef
khushi230 <khushboosirsa99@gmail.com>
2023-03-12 22:37:25 +0530

    navbar styling fixed

--- src/common/MobileNavbar.js
+++ src/common/MobileNavbar.js
@@ -24,46 +24,73 @@ const MobileNavbar = () => {
           </div>
         </div>
 
-        <div className={`nav-elements  ${showNavbar && "active"}`}>
-          <ul>
-            <li>
-              <Link className="text-white hover:text-yellow-300" to="/home">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link className="text-white hover:text-yellow-300" to="/charity">
-                Charity
-              </Link>
-            </li>
-            <li>
-              <Link className="text-white hover:text-yellow-300" to="/news">
-                News
-              </Link>
-            </li>
-            <li>
-              <Link className=" text-white hover:text-yellow-300" to="/events">
-                Events
-              </Link>
-            </li>
+        {showNavbar && (
+          <div
+            className={`bg-neutral-200  nav-elements  ${
+              showNavbar && "active"
+            }`}
+          >
+            <ul>
+              <li>
+                <Link
+                  className="text-neutral-600 hover:text-yellow-300"
+                  to="/home"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-neutral-600 hover:text-yellow-300"
+                  to="/charity"
+                >
+                  Charity
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-neutral-600 hover:text-yellow-300"
+                  to="/news"
+                >
+                  News
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-neutral-600 hover:text-yellow-300"
+                  to="/events"
+                >
+                  Events
+                </Link>
+              </li>
 
-            <li>
-              <Link className="text-white hover:text-yellow-300" to="/mission">
-                Mission
-              </Link>
-            </li>
-            <li>
-              <Link className="text-white hover:text-yellow-300" to="/about">
-                About Us
-              </Link>
-            </li>
-            <li>
-              <Link className="text-white hover:text-yellow-300" to="/contact">
-                Contact Us
-              </Link>
-            </li>
-          </ul>
-        </div>
+              <li>
+                <Link
+                  className="text-neutral-600 hover:text-yellow-300"
+                  to="/mission"
+                >
+                  Mission
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-neutral-600 hover:text-yellow-300"
+                  to="/about"
+                >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-neutral-600 hover:text-yellow-300"
+                  to="/contact"
+                >
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   );
